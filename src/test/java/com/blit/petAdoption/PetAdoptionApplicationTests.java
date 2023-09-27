9dfdcf606d7ed02259b59c1a4a53b357ec8e0435
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2023-09-27 14:28:36 -0400

    First commit

---
+++ src/test/java/com/blit/petAdoption/PetAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blit.petAdoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
