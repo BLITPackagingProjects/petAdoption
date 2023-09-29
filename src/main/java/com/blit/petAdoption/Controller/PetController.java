067c6e81c63ab3a5b29bde41b06125ea2f3cca2d
DNL0815 <dimitri.luck@outlook.com>
2023-09-29 11:18:43 -0400

    fixed api for all available pets, api is working now

--- src/main/java/com/blit/petAdoption/Controller/PetController.java
+++ src/main/java/com/blit/petAdoption/Controller/PetController.java
@@ -5,9 +5,7 @@ import com.blit.petAdoption.Service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,29 @@ public class PetController {
     @GetMapping
     public ResponseEntity<List<Pets>> viewAllPets(){
         return new ResponseEntity<>(petService.viewAllPets(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pets> viewPetById(@PathVariable Long id){
+        return new ResponseEntity<>(petService.retrievePetById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Pets> addPet(@RequestBody Pets pet){
+        return new ResponseEntity<>(petService.addPet(pet),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pets> updatePet(@PathVariable Long id, @RequestBody Pets pet){
+
+        Pets updatePet = petService.updatePet(id,pet);
+        return new ResponseEntity<>(updatePet, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletePet(@PathVariable Long id){
+        petService.deletePet(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
