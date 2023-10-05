c9c0650823b501540e979f689e826ccbc092e885
DNL0815 <dimitri.luck@outlook.com>
2023-10-05 17:48:43 -0400

    application api works

--- src/main/java/com/blit/petAdoption/Repository/CustomerRepo.java
+++ src/main/java/com/blit/petAdoption/Repository/CustomerRepo.java
@@ -3,9 +3,10 @@ package com.blit.petAdoption.Repository;
 import com.blit.petAdoption.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
-
+@CrossOrigin("*")
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByEmail(String email);
